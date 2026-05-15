6879ecd359de4ceea9a6be71698b2640285d4b4d
sakshamtbakshi <sakshambakshi@gmail.com>
2026-05-15 11:58:03 +0530

    worked on logger

--- src/middlewares/requestLogger.ts
+++ src/middlewares/requestLogger.ts
@@ -1,19 +1,41 @@
-import { Request, Response, NextFunction } from "express";
-import { logger } from "@/utils/logger";
+import { Request, Response, NextFunction } from 'express';
+import { logger } from '@/utils/logger';
 
-export const requestLogger = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const originalJson = res.json;
+export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+    const originalJson = res.json;
+    
+    // Intercept res.json to log the response immediately
+    res.json = function (body: any) {
+        const timestamp = new Date().toISOString();
+        const ip = req.ip || req.socket.remoteAddress;
+        
+        let status = 'success';
 
-  // Intercept res.json to log the response immediately
-  res.json = function (body: any) {
-    // log the request and response details here
+        const logEntry = {
+            timestamp,
+            status,
+            ip,
+            method: req.method,
+            path: req.path,
+            query: req.query,
+            responseBody: body
+        };
 
-    return originalJson.call(res, body); // Continue with the original response method
-  };
+        setImmediate(() => {
+            if(res.statusCode >= 400) {
+                if (body && typeof body.error === 'string' && body.error.includes('user_id')) {
+                    logEntry.status = 'getset fail';
+                } else {
+                    logEntry.status = 'fail';
+                }
+            } 
+            logger.info(logEntry);
+            
+        }); // reduce latency while sending the response  
 
-  next();
+        // Continue with the original response method
+        return originalJson.call(res, body);
+    };
+
+    next();
 };
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,18 @@
-import winston from "winston";
+import winston from 'winston';
+import 'winston-daily-rotate-file';
 
-export const logger = winston.createLogger({});
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+    filename: 'src/logs/app-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    maxFiles: process.env.LOG_ROTATION_TIME_LIMIT || '14d', // Keep logs for 14 days,
+    maxSize:'10mb'
+});
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console(),
+        fileRotateTransport
+    ]
+});
